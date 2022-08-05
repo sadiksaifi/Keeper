2a3690ed265d8c5f6127fb7eeed1b66db07f07b7
SDK <sadiksaifi205@gmail.com>
2022-08-05 22:33:19 +0530

    uploading Keeper App

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+let year = new Date().getFullYear();
+
+function Footer() {
+  return(
+  <footer>
+    <p>Copyright © {year}</p>
+  </footer>
+  )
+}
+
+export default Footer;
