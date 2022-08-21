4e4c49f4e5eaac50fa31788ba70fdd77d72a3c57
SDK <sadiksaifi205@gmail.com>
2022-08-22 02:24:07 +0530

    misc

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,26 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
+
+function createNote(noteItem) {
+  return (
+    <Note
+      key={noteItem.key}
+      title={noteItem.title}
+      content={noteItem.content}
+    />
+  );
+}
 
 function App() {
-  return(
+  return (
     <div>
       <Header />
-      <Note />
+      {notes.map(createNote)}
       <Footer />
     </div>
-  )
+  );
 }
 
 export default App;
